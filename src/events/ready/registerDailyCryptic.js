128c0a413d40e829eed799f920fe2400a3f43629
Trilleo <gongsanle@gmail.com>
2025-11-22 15:30:07 +0800

    0.6.4 Update: Cryptic Thread Listener & Bug Fixes

--- src/events/ready/registerDailyCryptic.js
+++ src/events/ready/registerDailyCryptic.js
@@ -125,7 +125,7 @@ module.exports = (client) => {
         }
 
         await channel.send({
-            content: "",
+            content: "<@#1417146582536814643>",
             embeds: [messageEmbedFirst, messageEmbedSecond],
         })
 
